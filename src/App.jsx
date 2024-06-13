34b98340fda68070d1130f24a20c802e802f3f4f
Dios <abhishekpatawari.codes@gmail.com>
2024-06-13 21:38:23 +0530

    add: functions

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import Header from './components/Header'
 import TransactionList from './components/TransactionList'
 import Balance from './components/Balance'
+import { GlobalProvider } from './context/GlobalState'
 import AddTransaction from './components/AddTransaction'
 import IncomeExpenses from './components/incomeExpenses'
 const App = () => {
   return (
-    <>
+    <GlobalProvider>
       <Header/>
       <div className="container">
         <Balance/>
@@ -14,7 +15,7 @@ const App = () => {
         <TransactionList/>
         <AddTransaction/>
       </div>
-    </>
+    </GlobalProvider>
   )
 }
 
